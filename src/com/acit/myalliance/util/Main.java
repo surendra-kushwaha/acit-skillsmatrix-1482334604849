5fca0b99e493c1bbf0073cb076d688a4514dd7a8
surendra.kushwaha <surendra.kushwaha@accenture.com>
2016-12-26 10:39:12 +0530

    debug

--- src/com/acit/myalliance/util/Main.java
+++ src/com/acit/myalliance/util/Main.java
@@ -1,34 +1,53 @@
 package com.acit.myalliance.util;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
 
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("'" + stringToHtmlString("1&May2016") + "'");
-        String userid="surendra.kushwaha@accenture.com";
-        if(userid.contains("@accenture.com")){
-        userid=userid.substring(0,userid.lastIndexOf("@"));
-        }
-        System.out.println("userid ::"+userid);
-        
-        /*System.out.println("'" + stringToHtmlString("<") + "'");
-        System.out.println("'" + stringToHtmlString(">") + "'");
-        System.out.println("'" + stringToHtmlString(" ") + "'");
-        System.out.println("'" + stringToHtmlString("     ") + "'");
-        System.out.println("'" + stringToHtmlString("&<>abc") + "'");
-        System.out.println("'" + stringToHtmlString("abc&<>") + "'");*/
-    }
-    public static final String stringToHtmlString(String s){
-       StringBuffer sb = new StringBuffer();
-       int n = s.length();
-       for (int i = 0; i < n; i++) {
-          char c = s.charAt(i);
-          switch (c) {
-             case '<': sb.append("&lt;"); break;
-             case '>': sb.append("&gt;"); break;
-             case '&': sb.append("&amp;"); break;
-             case '"': sb.append("&quot;"); break;
-             default:  sb.append(c); break;
-          }
-       }
-       return sb.toString();
-    }
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		String urlStr = "https://ts.accenture.com/sites/Accenture%20Innovation%20Center%20for%20IBM%20Technologies/_vti_bin//ListData.svc/ACITSkillsMatrix";
+		String domain = "DIR"; // May also be referred as realm
+		String userName = "surendra.kushwaha@accenture.com";
+		String password = "Dec@2016";		
+
+		String responseText = getAuthenticatedResponse(urlStr, domain, userName, password);
+
+	    System.out.println("response: " + responseText);
+	}
+
+	private static String getAuthenticatedResponse(final String urlStr, final String domain, final String userName, final String password) throws IOException {
+
+	    StringBuilder response = new StringBuilder();
+
+		Authenticator.setDefault(new Authenticator() {
+	        @Override
+	        public PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(domain + "\\" + userName, password.toCharArray());
+	        }
+	    });
+
+	    URL urlRequest = new URL(urlStr);
+	    HttpURLConnection conn = (HttpURLConnection) urlRequest.openConnection();
+	    conn.setDoOutput(true);
+	    conn.setDoInput(true);
+	    conn.setRequestMethod("GET");
+
+	    InputStream stream = conn.getInputStream();
+	    BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+	    String str = "";
+	    while ((str = in.readLine()) != null) {
+	        response.append(str);
+	    }
+	    in.close();		
+
+	    return response.toString();
+	}
+
 }
---
+++ src/wasdev/sample/servlet/KXExpiredAllianceService.java
@@ -0,0 +1,84 @@
+package wasdev.sample.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+
+
+/**
+ * Servlet implementation class KXService
+ */
+@WebServlet("/ExpiredAllianceService")
+public class KXExpiredAllianceService extends HttpServlet {
+	long serialVersionUID = 1L;
+	String protcol = "https";
+	int portNumber=443;
+	String domain="ts.accenture.com";
+	String pathEvents="/sites/Accenture%20Innovation%20Center%20for%20IBM%20Technologies/ACIT%20Microsite/_api/web/lists/getbytitle%28%27upcoming%20events%27%29/items%28%29";
+	
+	String pathAnnouncements="/sites/Accenture%20Innovation%20Center%20for%20IBM%20Technologies/ACIT%20Microsite/_vti_bin/ListData.svc/ACITAnnouncements";
+	String queryEvents="$top=10&$orderby=EventDate%20desc";
+	String queryAnnouncements="$top=10&$orderby=Modified%20desc";
+	String sharePointURL="https://ts.accenture.com/sites/AlliancesWizard/myAlliance/_vti_bin//ListData.svc/ExpiredAlliances";   
+   @Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	    InputStream Io=null;
+	    OutputStream out=null;
+	    String flag = request.getParameter("event");
+		try {
+			
+			Io = HttpClient.get(sharePointURL);
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin","*");
+			response.addHeader("Access-Control-Allow-Methods","GET,POST");
+			response.addHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+			out = response.getOutputStream();
+			Runtime runtime = Runtime.getRuntime();
+			String kxresponse = new String(IOUtils.toString(Io,"UTF-8"));
+			InputStream instream = IOUtils.toInputStream(kxresponse);
+			IOUtils.copyLarge(instream, out);
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally
+		{
+			try{
+				out.close();
+				Io.close();
+			}catch(Exception e){
+				
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
